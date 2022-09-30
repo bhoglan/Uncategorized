58c8c7c8b69645e77c1c83bb003e52ee0b06dfbe
Brian Hoglan <42455017+bhoglan@users.noreply.github.com>
2022-09-30 15:02:23 -0700

    Update memoryinfo.sh

--- memoryinfo.sh
+++ memoryinfo.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 # This is specifically meant to run on an ESXi host that doesn't posess the dmidecode command.
 # Run smbiosDump to collect the data then Sedify this thing and pull out the data we want
-smbiosDump | sed -nr \
-        '/^(Memory\ Device\|Size\:\|Form\ Factor\:\|Locator\:\|Speed\:\|Serial\ Number\:|Part\ Number\:)$/p' \
-        >> /tmp/memorySlots.txt
+smbiosDump | sed \
+	-e '/Memory\sDevice/b' -e '/Size\:/b' -e '/Form\sFactor\:/b' -e '/Locator\:/b' -e '/Speed\:/b' -e '/Serial\sNumber\:/b' -e '/Part\sNumber:/b' -e d \
+	>> /tmp/memorySlots.txt
