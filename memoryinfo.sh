700d167e3ba7d81bafceac225f7ddb5ce7252625
Brian Hoglan <42455017+bhoglan@users.noreply.github.com>
2022-09-30 10:16:01 -0700

    Update memoryinfo.sh

--- memoryinfo.sh
+++ memoryinfo.sh
@@ -4,4 +4,4 @@
 # Run smbiosDump to collect the data then Sedify this thing and pull out the data we want
 smbiosDump | sed -nr \
         '/^(Memory\ Device\|Size\:\|Form\ Factor\:\|Locator\:\|Speed\:\|Serial\ Number\:|Part\ Number\:)$/p' \
-        >> /root/memorySlots.txt
+        >> /tmp/memorySlots.txt
