daa362840e24c5706caa77d978d3e1012a42ac43
Brian Hoglan <42455017+bhoglan@users.noreply.github.com>
2022-10-05 08:28:49 -0700

    Create md5compare.sh

---
+++ md5compare.sh
@@ -0,0 +1,3 @@
+#Placeholder, script to read a known md5 (or sha) hash from the user, store as a variable, then run the same hash against a filename specified by the user. Compare the two hashes and ouput the result.
+##Option flags for different hash mechanisms? 
+##Regex matching of known hash patterns? Each algorithm should produce a unique pattern.
